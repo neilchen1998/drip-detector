ddbd7e87c6ffada311def6f29d3b5e0f7c1a4a9b
Neil Chen <neilchen1998@gmail.com>
2025-11-10 21:54:40 -0600

    fed batches of imgs to the model

--- inference.py
+++ inference.py
@@ -1,42 +1,61 @@
 from ultralytics import YOLO
 from pathlib import Path
+import sys
 
-def inference(pic_path: str, weights_path: str):
-    """Inferences the picture with the pretrained model
+def check_image_directory(pic_dir_path: str):
+    """Checks if the given picture directory exists and contains at least one image
 
     Parameter:
-        pic_path: the file path of the picture
-        weights_path: the file path of the weights
+        pic_dir_path: the directory of the picture
     """
 
-    # Check if the picture exists
-    if Path(pic_path).exists() == False:
-        print(f"Error: {pic_path} does not exist.\n")
+    pic_dir = Path(pic_dir_path)
+
+    if not pic_dir.exists() or not pic_dir.is_dir():
+        print(f"Error: {pic_dir_path} does not exist.\n")
+        return False
+
+    if not any(f.suffix.lower() in ['.jpg', '.jpeg', '.png'] for f in pic_dir.iterdir() if f.is_file()):
+        print(f"Error: {pic_dir_path} does not contain at least one image.\n")
+        return False
+
+    return True
+
+def main():
+
+    pic_dir_path = "inference/"
+    weights_path = "runs/detect/yolov8n_fashionpedia13/weights/best.pt"
+
+    # Check if the weight file exists
+    if not check_image_directory(pic_dir_path):
         return
 
     # Check if the weight file exists
-    if Path(weights_path).exists() == False:
+    if not Path(weights_path).exists():
         print(f"Error: {weights_path} does not exist.\n")
         return
 
-    # Load the weights and predict the given picture
+    # Load the model
     model = YOLO(weights_path)
-    results = model.predict(pic_path)
 
-    # Print the number of bounding boxes
-    if results[0].boxes:
-        print(f"Number of detected boxes: {len(results[0].boxes)}")
-    else:
-        print("No objects detected in this image.")
+    # Predict the images in batches
+    results = model.predict(Path(pic_dir_path), conf=0.4)
 
-    # Show the annotated picture
-    results[0].show()
+    for i, result in enumerate(results):
+
+        # Retreive the filename
+        img_path = result.path
+        filename = Path(img_path).name
+
+        # Only show images that there is at least one hit
+        if result.boxes and len(result.boxes) > 0:
+            print(f"{len(result.boxes)} object(s) detected in {filename}.")
+            result.show()
+        else:
+            print(f"No objects detected in {filename}.")
 
     return
 
 if __name__ == '__main__':
 
-    pic_path = "pic_1.jpg"
-    weights_path = "runs/detect/yolov8n_fashionpedia14/weights/best.pt"
-
-    inference(pic_path, weights_path)
+    main()
